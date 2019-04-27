3c193c87184d8345af2cc7a7a4a17d9136955173
cnzxl <89454944@qq.com>
2019-04-27 11:14:36 +0800

    66

--- test3/WebForm1.aspx.cs
+++ test3/WebForm1.aspx.cs
@@ -12,7 +12,7 @@ namespace test3
         protected void Page_Load(object sender, EventArgs e)
         {
             int zz=999;
-            int i = 67;
+            int i = 7;
             int b = 20;
         }
     }
